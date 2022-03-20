3bcce88f3b26c104f517ee1a2db8c7763b6efd7b
Shantanu Ray <chico.chego@gmail.com>
2022-03-20 12:07:54 -0700

    Revert "corrected indexing of last spout contact"
    This reverts commit bb4f1e56bc92fdf41a867e92ff4d3743d1246b0b.

--- trial_segmentation.m
+++ trial_segmentation.m
@@ -121,11 +121,11 @@ for j=1:length(spoutContact_on_first)
 		last_sc_indx = find(videoFrames_timestamps(sc_indx)<=spoutContact_on_multi{j}(end));
 		% In case there is only one spout contact, use the first sc
 		if isempty(last_sc_indx)
-			last_sc_indx = 1;
+			last_sc_indx = sc_indx(1);
 		end
 		%selects the first value of the time stamp, this is the closest frame timestamp to the resp. spout contact
-		end_ts_last = [end_ts_last; videoFrames_timestamps(sc_indx(1)+last_sc_indx(end)-1)]; 
-		end_idx_last = [end_idx_last; sc_indx(1)+last_sc_indx(end)-1];
+		end_ts_last = [end_ts_last; videoFrames_timestamps(last_sc_indx(1))]; 
+		end_idx_last = [end_idx_last; last_sc_indx(1)];
 	else
 		% For no spout contact, init to nan
 		end_ts_first = [end_ts_first; nan];
