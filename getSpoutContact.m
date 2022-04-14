51809927a34996ea7617fdbee93c9a3a060e638b
Shantanu Ray <chico.chego@gmail.com>
2022-04-14 14:55:21 -0700

    Added check for perch contact at start

--- getSpoutContact.m
+++ getSpoutContact.m
@@ -1,4 +1,4 @@
-function [spoutContact_on_first, spoutContact_on_multi, hitormiss] = getSpoutContact(startTrialTrigger, spoutContact_on)
+function [spoutContact_on_first, spoutContact_on_multi, hitormiss, perchOnStart] = getSpoutContact(startTrialTrigger, spoutContact_on, perchContact_cont)
 	% [spoutContact_on_first, spoutContact_on_multi, hitormiss] = getSpoutContact(startTrialTrigger, spoutContact_on)
 	% Inputs:
 	%	- startTrialTrigger: solenoid_on or tone_on events - Marks the start of trial
@@ -9,9 +9,13 @@ function [spoutContact_on_first, spoutContact_on_multi, hitormiss] = getSpoutCon
 	% 	- spoutContact_on_multi: All spout contacts between each startTrialTrigger and before next startTrialTrigger
 	%	- hitormiss: If no spout contact for given startTrialTrigger (1 - hit; 0 - miss)
 
+	if nargin<3
+		perchContact_cont = [];
+	end
 	spoutContact_on_first = [];% creates an empty array for the first spout contact after each reward presentation (startTrialTrigger) which can be filled with values generated below
 	spoutContact_on_multi = cell(length(startTrialTrigger), 1);% creates an empty array for the all spout contacts after each reward presentation (startTrialTrigger) and before next reward presentation
 	hitormiss = zeros(length(startTrialTrigger), 1);
+	perchOnStart = zeros(length(startTrialTrigger), 1); % By default, assume hand on perch at start of trial
 	for i = 1:length(startTrialTrigger) % starts a for loop which will cycle through all the values in startTrialTrigger
 		first_sc_index = find(spoutContact_on > startTrialTrigger(i)); %defines a variable tmp_index that for all values of solenoid on finds the value in spoutContact_on that is higher
 		if isempty(first_sc_index)
@@ -39,6 +43,9 @@ function [spoutContact_on_first, spoutContact_on_multi, hitormiss] = getSpoutCon
 			end
 			spoutContact_on_first = [spoutContact_on_first; curr_spoutContact_on]; % for the last value, do not check the next value because it will be empty 
 			spoutContact_on_multi{i} = [curr_spoutContact_on];
+		end
+		if sum(startTrialTrigger(i) == find(perchContact_cont)) > 0
+			perchOnStart(i) = 1;
 		end
 	end
 end
