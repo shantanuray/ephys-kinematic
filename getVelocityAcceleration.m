82ae043a235a947f10f2ef8fe57f2d2e46b77dc5
Shantanu Ray <chico.chego@gmail.com>
2022-06-16 15:30:41 -0700

    Generaized centrl derivative & added jerk

--- getVelocityAcceleration.m
+++ getVelocityAcceleration.m
@@ -11,15 +11,15 @@ function trial_list = getVelocityAcceleration(trial_list, fs, processLabels, num
 % 					(Default) {'aniposeData_fixed_relative',...
 % 					 			'aniposeData_first_sc_relative',...
 % 					 			'aniposeData_last_sc_relative'};
-% 	- num_points  int, optional (3,5,7,9)
-%     			  Number of points to use, must be odd. Default = 3. (5/7/9/default)
+% 	- num_points  int array, Number of points to use respectively for 1st, 2nd and 3rder order derivative
+%				  Must be odd and > order of derivative + 1. Default = [3,5,7]
 % Examples
 % trial_list = getVelocityAcceleration(trial_list, 300);
 % trial_list = getVelocityAcceleration(trial_list, 300, {'aniposeData_fixed'}, 5);
 
 % Data Labels to compute velocity and acceleration
 if nargin<4
-	num_points = 3;
+	num_points = [3, 5, 7];
 end
 if nargin<3
 	processLabels = {'aniposeData_fixed_relative',...
@@ -36,8 +36,9 @@ for trial_idx = 1:length(trial_list)
 		%	- x,y,z wrt reference (water spout)
 		%	- r, azi, el (of x,y,z wrt reference)
 		% Vel & Accl contain the 1st & 2nd central derivative of the above
-		velocity = central_derivative(data, fs, 1, num_points); % Velocity: order = 1
-		acceleration = central_derivative(data, fs, 2, num_points); % Acceleration: order = 2
+		velocity = central_derivative(data, fs, 1, num_points(1)); % Velocity: order = 1
+		acceleration = central_derivative(data, fs, 2, num_points(2)); % Acceleration: order = 2
+		jerk = central_derivative(data, fs, 3, num_points(3)); % Acceleration: order = 2
 		%% The above derivative of r (dr/dt) implies rate of change of relative displacement to reference
 		%% and not the change from current to next position
 		%% For the speed between two points, i.e. relative distance/ time between two positions,
@@ -63,6 +64,10 @@ for trial_idx = 1:length(trial_list)
 		d2xyzdt2 = acceleration(:, xyzLocation);
 		d2xyzdt2 = reshape(d2xyzdt2, size(d2xyzdt2,1), 3, length(xLocation));
 		xyzAcc = reshape(sqrt(sum(d2xyzdt2.^2, 2)), size(d2xyzdt2,1), length(xLocation));
+		% Get jerk between two points
+		d3xyzdt3 = jerk(:, xyzLocation);
+		d3xyzdt3 = reshape(d3xyzdt3, size(d3xyzdt3,1), 3, length(xLocation));
+		xyzJerk = reshape(sqrt(sum(d3xyzdt3.^2, 2)), size(d3xyzdt3,1), length(xLocation));
 		trial_list(trial_idx).(strcat(processLabels{label_idx}, '_velocity')) = array2table([velocity,...
 																							 xyzSpeed,...
 																							 azimuth_dxyzdt,...
@@ -75,5 +80,9 @@ for trial_idx = 1:length(trial_list)
 																								 xyzAcc],...
 																								'VariableNames', [colNames,...
 																											  	  strcat(colNames(xLocation), 'yzAcc')]);
+		trial_list(trial_idx).(strcat(processLabels{label_idx}, '_jerk')) = array2table([jerk,...
+																						 xyzJerk],...
+																						 'VariableNames', [colNames,...
+																										   strcat(colNames(xLocation), 'yzJerk')]);
 	end
 end
