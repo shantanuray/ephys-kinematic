0f4c87592f240d81c66aa89b488abd09b04ca9df
Shantanu Ray <chico.chego@gmail.com>
2022-06-20 18:42:44 -0700

    Added radius of curvature to relative

--- getRelativeDistance.m
+++ getRelativeDistance.m
@@ -50,19 +50,27 @@ for trial_idx = 1:length(trial_list)
 			dataxyz = reshape(dataxyz, size(dataxyz,1), 3, length(xLocation));  % Reshape to #samples x 3 x bodyparts
 			xyzRel = dataxyz-dataref;
 			[azimuth_rel, elevation_rel, r_rel] = cart2sph(xyzRel(:,1,:),xyzRel(:,2,:),xyzRel(:,3,:));
-			dataRel = cat(2, xyzRel, r_rel, azimuth_rel, elevation_rel);
-			dataRel = reshape(dataRel, size(dataRel,1), size(dataRel,2)*size(dataRel,3)); % Flatten it back
+			[l, roc, k] = arrayfun(@(i)curvature(xyzRel(:,:,i)), 1:size(xyzRel, 3), 'UniformOutput', false);
+			arc_length = cat(2, l{:,:});
+			arc_length = reshape(arc_length, size(arc_length,1), 1, size(arc_length,2));
+			radius_of_curvature = cat(2, roc{:,:});
+			radius_of_curvature = reshape(radius_of_curvature, size(radius_of_curvature,1), 1, size(radius_of_curvature,2));
+			dataRel = cat(2, xyzRel, r_rel, azimuth_rel, elevation_rel, arc_length, radius_of_curvature);
+			num_labels = size(dataRel,2);
+			dataRelFlat = reshape(dataRel, size(dataRel,1), size(dataRel,2)*size(dataRel,3)); % Flatten it back
 			for bodyPart_idx = 1:length(xLocation)
 				base_label = colNames{xLocation(bodyPart_idx)}(1:strfind(colNames{xLocation(bodyPart_idx)}, '_x')-1);
-				dataRel_labels{(bodyPart_idx-1)*6+1} = strcat(base_label, '_x');
-				dataRel_labels{(bodyPart_idx-1)*6+2} = strcat(base_label, '_y');
-				dataRel_labels{(bodyPart_idx-1)*6+3} = strcat(base_label, '_z');
-				dataRel_labels{(bodyPart_idx-1)*6+4} = strcat(base_label, '_r');
-				dataRel_labels{(bodyPart_idx-1)*6+5} = strcat(base_label, '_azimuth');
-				dataRel_labels{(bodyPart_idx-1)*6+6} = strcat(base_label, '_elevation');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+1} = strcat(base_label, '_x');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+2} = strcat(base_label, '_y');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+3} = strcat(base_label, '_z');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+4} = strcat(base_label, '_r');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+5} = strcat(base_label, '_azimuth');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+6} = strcat(base_label, '_elevation');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+7} = strcat(base_label, '_arc_length');
+				dataRel_labels{(bodyPart_idx-1)*num_labels+8} = strcat(base_label, '_radius_of_curvature');
 			end
-			trial_list(trial_idx).(strcat(processLabels{label_idx}, '_relative')) = array2table(dataRel,...
-																								'VariableNames', dataRel_labels);
+			trial_list(trial_idx).(strcat(processLabels{label_idx}, '_relative')) = ...
+			array2table(dataRelFlat, 'VariableNames', dataRel_labels);
 		else
 			trial_list(trial_idx).(strcat(processLabels{label_idx}, '_relative')) = array2table([]);
 		end
