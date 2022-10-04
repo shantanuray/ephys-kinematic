06942180f70363e97b42aa5860862707aaf12ca9
Ayesha <ayeshat90@gmail.com>
2022-10-04 09:08:28 -0700

    Added condition to check loc before num

--- ropepull/getSegmentNumPeaks.m
+++ ropepull/getSegmentNumPeaks.m
@@ -1,9 +1,13 @@
 function numPeaks = getSegmentNumPeaks(trial, colName)
 	if isfield(trial.(colName), 'data')
 		data = trial.(colName).('data');
-		assert(size(data,2)==1, 'Data should be 1-D');
-		[pks, loc] = findpeaks(data);
-		numPeaks = length(loc);
+        if ~isnan(data)
+		    assert(size(data,2)==1, 'Data should be 1-D');
+		    [~, loc] = findpeaks(data);
+		    numPeaks = length(loc);
+        else
+            numPeaks = 0;
+        end
 	else
 		numPeaks = 0;
 	end
