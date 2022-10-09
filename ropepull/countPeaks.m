539cb51183cf639136c8070b49da268a96109404
Ayesha <ayeshat90@gmail.com>
2022-10-09 12:36:27 -0700

    add 20 samples 100ms on either side of the peaks

--- ropepull/countPeaks.m
+++ ropepull/countPeaks.m
@@ -5,7 +5,7 @@ function [num_pks, freq_pks] = countPeaks(trial, markerName, dorsoVentralAxis, m
 	% t = duration of pulling action (not necessarily the entire trial)
 	loc = getPeaks(trial, markerName, dorsoVentralAxis, maxPeakHeight, varargin{:});
 	if ~isempty(loc)
-		t = (loc(end)-loc(1))/trial.video_fps;
+		t = ((loc(end)+20)-(loc(1)-20))/trial.video_fps; % add 20 samples 100ms on either side of the peaks 
 		num_pks = length(loc);
 		freq_pks = num_pks/t;
 	else
