2fbf8ec2c4beb68eeb5a333eb4fac7f335af8166
Ayesha <ayeshat90@gmail.com>
2023-03-15 22:03:28 -0700

    Updated gripAperture readme

--- getGripAperture.m
+++ getGripAperture.m
@@ -1,23 +1,14 @@
 function trial_list = getGripAperture(trial, processLabel, digit1label, digit2label)
-    % Usage: getGripAperture(trial_list, dataLabels, digit1label,digit2label);
+    % Usage: getGripAperture(trial_list, processLabel, digit1label, digit2label);
     % calculates the distance between digits specified, for classical grip aperture  calculations the tip of d2 and d5 would be used , but other markers such as the knuckle or other digits coulber be considered 
     % Input:
     %   - trial_list: list of segmented trials with reach data (see trial_segmentation.m)
-    %   - dataLabels: cell array with data label names
-    %                 eg. {'anipose_fixed'} or
-    %                     {'anipose_fixed', 'anipose_first_sc'}
-    %                 (default: {'aniposeData_fixed',...
-    %                           'aniposeData_first_sc',...
-    %                           'aniposeData_last_sc'})
-    %   - digit1label:   string indicating digit1label (default: 'right_d5_tip')
-    %   - digit2label:   string indicating digit2label (default:  'right_d2_tip')
+    %   - processLabel: str, Default = 'aniposeData_fixed_relative'
+    %   - digit1label:   string indicating digit1label (default: 'right_d5_tip_r')
+    %   - digit2label:   string indicating digit2label (default:  'right_d2_tip_r')
     % Examples
-    % trial_list = getRelativeDistance(trial_list, {'aniposeData_fixed'});
-    % trial_list = getRelativeDistance(trial_list, {'aniposeData_fixed'}, 'right_d5_tip','right_d2_tip');
-
-    % Assumption: Data is stored in a table in the following structure for each body part/ reference
-    %   left_wrist_x,left_wrist_y,left_wrist_z,left_wrist_error,left_wrist_ncams,left_wrist_score,left_d1_knuckle_x,...
-    %   Calculate and save relative distance for x,y,z only
+    % trial_list = getGripAperture(trial_list, 'aniposeData_fixed_relative'});
+    % trial_list = getGripAperture(trial_list, 'aniposeData_fixed_relative', 'right_d5_tip_r','right_d2_tip_r');
 
 
     if nargin<4
