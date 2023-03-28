f831c70529543b6c4e82a878a624072edfefef64
Ayesha <ayeshat90@gmail.com>
2023-03-28 13:49:06 -0700

    final get Summary info

--- getSummaryInfo_03272023.m
+++ getSummaryInfo_03272023.m
@@ -75,11 +75,7 @@ function summaryTable = getSummaryInfo(trial_list, fs, outputLabel, bodyPart, pr
 		% on/off
 		lightTrig{trial_idx, 1} = trial.lightTrig;
 		[azi_1,elev_1]=getAngleRefPosition(trial,distDataLabel,poseIDAngleRefPosition,startPos,endPos);
-		azi_reachAngle = [azi_reachAngle;azi_1];
-		elev_reachAngle = [elev_reachAngle;elev_1];
 		[azi_2,elev_2]=getAngleRefPosition(trial,distDataLabel,poseIDAngleRefPosition,startPos,endPos_speedMax);
-		azi_aimAngle=[azi_aimAngle;azi_2];
-		elev_aimAngle=[elev_aimAngle;elev_2];
 		% Get speed and distance for every trial
 		xDistTrial = nan; yDistTrial = nan; zDistTrial = nan; totalDistTrial = nan;
 		xSpeedTrial = nan; ySpeedTrial = nan; zSpeedTrial = nan; averageSpeedTrial = nan;
@@ -112,30 +108,19 @@ function summaryTable = getSummaryInfo(trial_list, fs, outputLabel, bodyPart, pr
 		end
 		averageSpeedTrial = totalDistTrial/totalTimeTrial;
 		maxSpeedTrial = max(xyzSpeedTrialPt);
-		distRelMaxSpeed=trial.distDataLabel.distDataLabel(speedMax_idx)
+		distRelMaxSpeed=trial.(distDataLabel).(rColLabel)(speedMax_idx)
 		rRelTrialMaxSpeed = trial.(distDataLabel).(rColLabel)(max_loc);
 		xSpeedTrial = xDistTrial/totalTimeTrial;
 		ySpeedTrial = yDistTrial/totalTimeTrial;
 		zSpeedTrial = zDistTrial/totalTimeTrial;
-        wrist_endpoint_trial=trial.distDataLabel.poseIDAngleRefPosition(endPos);
-        d3_knuckle_endpoint_trial=trial.distDataLabel.poseID_endPointref2(endPos);
-        wrist_endpoint=[wrist_endpoint;wrist_endpoint_trial];
-        d3_knuckle_endpoint_trial=[d3_knuckle_endpoint;d3_knuckle_endpoint_trial];
-        start_AP_trial = trial.distDataLabel.([poseIDAngleRefPosition(1).xColLabel]);
-        start_AP_trial =start_AP_trial(1);
-        start_ML_trial = trial.distDataLabel.([poseIDAngleRefPosition(1).yColLabel]);
-        start_ML_trial= start_ML_trial(1);
-        start_DV_trial = trial.distDataLabel.([poseIDAngleRefPosition(1).zColLabel]);
-        start_DV_trial=start_DV_trial(1);
-        start_r_trial=trial.distDataLabel.poseIDAngleRefPosition;
-        start_r_trial=start_r(1);
-        start_AP=[start_AP;start_AP_trial];
-        start_ML=[start_ML;start_ML_trial];
-        start_DV=[start_DV;start_DV_trial];
-        start_r=[start_r;start_r_trial];
-        tortuosity_trial=getTortuosity(trial,distDataLabel,[poseIDAngleRefPosition,rColLabel]);
-        tortuosity=[tortuosity;tortuosity_trial];
-        [accPk_count_trial;, accPk_meanAmp_trial, accPk_maxAmp_trial, accPk_meanProm_trial, accPk_maxProm_trial, accPk_meanWidth_trial, accPk_maxWidth_trial,accPk_freq_trial,acc_period_trial]=getAccPkInfo(trial);
+        wrist_endpoint_trial=trial.(distDataLabel).(rColLabel)(endPos);
+        d3_knuckle_endpoint_trial=trial.(distDataLabel).(poseID_endPointref2)(endPos);
+        start_AP_trial = trial.(distDataLabel).(xColLabel)(1);
+        start_ML_trial = trial.(distDataLabel).(yColLabel)(1);
+        start_DV_trial = trial.(distDataLabel).(zColLabel)(1);
+        start_r_trial=trial.(distDataLabel).(rColLabel)(1);
+        tortuosity_trial=getTortuosity(trial,distDataLabel,rColLabel);
+        [accPk_count_trial, accPk_meanAmp_trial, accPk_maxAmp_trial, accPk_meanProm_trial, accPk_maxProm_trial, accPk_meanWidth_trial, accPk_maxWidth_trial,accPk_freq_trial,acc_period_trial]=getAccPkInfo(trial);
         if strcmpi(trial.lightTrig,'ON')
            LightONtime_ms_trial= (trial.lightOnTrig_ts(1)-(trial.start_ts+trial.reach_start_ts))*1000/30000;
         elseif strcmpi(trial.lightTrig,'OFF')
@@ -160,6 +145,19 @@ function summaryTable = getSummaryInfo(trial_list, fs, outputLabel, bodyPart, pr
 		xSpeed = [xSpeed; xSpeedTrial]; ySpeed = [ySpeed; ySpeedTrial]; zSpeed = [zSpeed; zSpeedTrial];
 		averageSpeed = [averageSpeed; averageSpeedTrial];
 		maxSpeed = [maxSpeed; maxSpeedTrial]; distRelMaxSpeed = [distRelMaxSpeed; rRelTrialMaxSpeed];
+		wrist_endpoint=[wrist_endpoint;wrist_endpoint_trial];
+        d3_knuckle_endpoint_trial=[d3_knuckle_endpoint;d3_knuckle_endpoint_trial];
+        azi_reachAngle = [azi_reachAngle;azi_1];
+		elev_reachAngle = [elev_reachAngle;elev_1];
+		azi_aimAngle=[azi_aimAngle;azi_2];
+		elev_aimAngle=[elev_aimAngle;elev_2]
+        start_AP=[start_AP;start_AP_trial];
+        start_ML=[start_ML;start_ML_trial];
+        start_DV=[start_DV;start_DV_trial];
+        start_r=[start_r;start_r_trial];
+        azi_aimAngle=[azi_aimAngle;azi_2];
+		elev_aimAngle=[elev_aimAngle;elev_2];
+		tortuosity=[tortuosity;tortuosity_trial];
 		accPk_count=[accPk_count;accPk_count_trial]; accPk_meanAmp=[accPk_meanAmp;accPk_meanAmp_trial];accPk_maxAmp=[accPk_maxAmp;accPk_maxAmp_trial]; accPk_meanProm=[accPk_meanProm;accPk_meanProm_trial]; accPk_maxProm=[accPk_maxProm;accPk_maxProm_trial]; accPk_meanWidth=[accPk_meanWidth;accPk_meanWidth_trial]; accPk_maxWidth=[accPk_maxWidth;accPk_maxWidth_trial]; accPk_freq=[accPk_freq;accPk_freq_trial]; acc_period=[acc_period;acc_period_trial];LightONtime_ms=[LightONtime_ms;LightONtime_ms_trial];
 	end
 	summaryTable = table(label,...
